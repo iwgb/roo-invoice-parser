3c6cfde4fb134a8071d14fb8fb2e9f9e3512b2ab
Guy Mac <hello@guymac.eu>
2021-07-26 20:27:43 +0100

    :sparkles: export supported markets

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import parseInvoice from './parse';
 import processInvoices from './process';
+import marketParsers from './market/markets';
+
+const markets = Object.keys(marketParsers);
 
 export {
+  markets,
   parseInvoice,
   processInvoices,
 };
